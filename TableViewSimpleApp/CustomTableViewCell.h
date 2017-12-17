187fb20870da317aa380d4783f0b787db1b9380f
Anton Poluyanov <poluyanov80@gmail.com>
2017-12-17 20:46:23 +0300

    Few changes

---
+++ TableViewSimpleApp/CustomTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.h
+//  TableViewSimpleApp
+//
+//  Created by Антон Полуянов on 17/12/2017.
+//  Copyright © 2017 Антон Полуянов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+
+@end
---
+++ TableViewSimpleApp/CustomTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.m
+//  TableViewSimpleApp
+//
+//  Created by Антон Полуянов on 17/12/2017.
+//  Copyright © 2017 Антон Полуянов. All rights reserved.
+//
+
+#import "CustomTableViewCell.h"
+
+@implementation CustomTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
